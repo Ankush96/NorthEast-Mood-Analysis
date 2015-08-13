71140c2eccbc1af7db7b7ffaec891e96e27f9013
Ankush Chatterjee <ankushchatterjee.iitkgp@gmail.com>
2015-08-14 01:27:40 +0530

    ZeeNews added

--- Sentiment Analysis/Extract-articles.R
+++ Sentiment Analysis/Extract-articles.R
@@ -1,16 +1,40 @@
+library("stringr")
 heading={}
 content={}
 date={}
-for (i in 1:length(links)){
-  a<-html(links[i])
+#---------------------------NorthEastToday-----------------------------------
+
+for (i in 1:length(links1)){
+  a<-html(links1[i])
   heading[i]<-a %>% html_node(".content-article-title h1") %>% html_text()    #Read heading
   print(heading[i])                               
   content_list<-a %>% html_nodes(".shortcode-content p") %>% html_text()      #Takes each paragraph as an item in a list
   content[i]<-paste(content_list,sep=" ",collapse = " ")
-  #print(content[i])
+  print(content[i])
   date[i]<-a %>% html_node(".calendar-date") %>% html_text()
 }
 df<-data.frame(heading,date,content)
 names(df)<-c("Heading","Date","Content")
 write.csv(df,"NortheastToday.csv")
 print("Exported to csv format")
+
+#---------------------------------------------------------------------
+
+#---------------------------ZeeNews-----------------------------------
+
+for (i in 1:length(links2)){
+  a<-html(links2[i])
+  heading[i]<-a %>% html_node(".full-story-head h1") %>% html_text()    #Read heading
+  print(heading[i])                               
+  content_list<-a %>% html_nodes(".even p") %>% html_text()      #Takes each paragraph as an item in a list
+  content[i]<-paste(content_list,sep=" ",collapse = "")
+  print(content[i])
+  date[i]<-a %>% html_node(".writer") %>% html_text()
+  date[i]<-paste(word(date[i],-5),word(date[i],-4),word(date[i],-3))
+  print(date[i])
+}
+df<-data.frame(heading,date,content)
+names(df)<-c("Heading","Date","Content")
+write.csv(df,"ZeeNews.csv")
+print("Exported to csv format")
+#---------------------------------------------------------------------
--- Sentiment Analysis/News_links_scraper.R
+++ Sentiment Analysis/News_links_scraper.R
@@ -1,15 +1,32 @@
 library("rvest")
 # Sys.setenv(http_proxy="10.3.100.207:8080") 
-news_links<-html("http://www.northeasttoday.in/")
 
-links<-news_links %>%
+#--------------------NorthEastToday--------------------------------
+
+news_links<-html("http://www.northeasttoday.in/")
+  links1<-news_links %>%
   html_nodes(".content-category a ") %>%
   html_attr("href")
 
-half = length(links)/2
+half = length(links1)/2
 a = (1:half)
 a= 2*a
 a=a-1
-links<-links[a]
+links1<-links1[a]
 
-#print(links)
+print(links1)
+
+#---------------------------------------------------------------------
+
+#---------------------------ZeeNews-----------------------------------
+
+news_links<-html("http://zeenews.india.com/state/north-east")
+links2<-news_links %>%
+  html_nodes(".lead-health-ab a") %>%
+  html_attr("href")
+for(i in 1:length(links2))
+{
+  links2[i]=paste("http://zeenews.india.com",links2[i],sep="")
+}
+print(links2)
+#---------------------------------------------------------------------
