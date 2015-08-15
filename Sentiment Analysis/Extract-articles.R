db008de88fb8c647315922064d3fbb3605e1c1e4
Ankush Chatterjee <ankushchatterjee.iitkgp@gmail.com>
2015-08-15 13:22:47 +0530

    Statewise news from NorthEastToday

--- Sentiment Analysis/Extract-articles.R
+++ Sentiment Analysis/Extract-articles.R
@@ -7,11 +7,12 @@ date={}
 for (i in 1:length(links1)){
   a<-html(links1[i])
   heading[i]<-a %>% html_node(".content-article-title h1") %>% html_text()    #Read heading
-  print(heading[i])                               
+  print(paste(i,"->",heading[i]))                               
   content_list<-a %>% html_nodes(".shortcode-content p") %>% html_text()      #Takes each paragraph as an item in a list
   content[i]<-paste(content_list,sep=" ",collapse = " ")
-  print(content[i])
+  #print(content[i])
   date[i]<-a %>% html_node(".calendar-date") %>% html_text()
+  #print(date[i])
 }
 df<-data.frame(heading,date,content)
 names(df)<-c("Heading","Date","Content")
@@ -22,19 +23,20 @@ print("Exported to csv format")
 
 #---------------------------ZeeNews-----------------------------------
 
-for (i in 1:length(links2)){
-  a<-html(links2[i])
-  heading[i]<-a %>% html_node("h1") %>% html_text()    #Read heading
-  print(heading[i])                               
-  content_list<-a %>% html_nodes(".even p") %>% html_text()      #Takes each paragraph as an item in a list
-  content[i]<-paste(content_list,sep=" ",collapse = "")
-  print(content[i])
-  date[i]<-a %>% html_node(".writer") %>% html_text()
-  date[i]<-paste(word(date[i],-5),word(date[i],-4),word(date[i],-3))
-  print(date[i])
-}
-df<-data.frame(heading,date,content)
-names(df)<-c("Heading","Date","Content")
-write.csv(df,"ZeeNews.csv")
-print("Exported to csv format")
+# for (i in 1:length(links2)){
+#   a<-html(links2[i])
+#   heading[i]<-a %>% html_node("h1") %>% html_text()    #Read heading
+#   print(heading[i])                               
+#   content_list<-a %>% html_nodes(".even p") %>% html_text()      #Takes each paragraph as an item in a list
+#   content[i]<-paste(content_list,sep=" ",collapse = "")
+#   print(content[i])
+#   date[i]<-a %>% html_node(".writer") %>% html_text()
+#   date[i]<-paste(word(date[i],-5),word(date[i],-4),word(date[i],-3))
+#   print(date[i])
+# }
+# df<-data.frame(heading,date,content)
+# names(df)<-c("Heading","Date","Content")
+# write.csv(df,"ZeeNews.csv")
+# print("Exported to csv format")
+
 #---------------------------------------------------------------------
--- Sentiment Analysis/News_links_scraper.R
+++ Sentiment Analysis/News_links_scraper.R
@@ -1,39 +1,64 @@
 library("rvest")
 # Sys.setenv(http_proxy="10.3.100.207:8080") 
+states<-c("arunachal-pradesh","assam","manipur","meghalaya","mizoram","nagaland","tripura")
 
 #--------------------NorthEastToday--------------------------------
-'''
-news_links<-html("http://www.northeasttoday.in/")
-  links1<-news_links %>%
-  html_nodes(".content-category a ") %>%
-  html_attr("href")
+links1={}
+url_name<-"http://www.northeasttoday.in/category/"
+for(s_no in 1:length(states))
+{
+  
+  print(paste("Currently in",states[s_no]))
+  
+  #------------------Accessing the number of pages----------------------
+  exact_url<-paste(url_name,states[s_no],"/",sep="")
+  news_links<-html(exact_url)
+  max_pages<-news_links %>%
+    html_nodes(".page-numbers") %>%
+    html_text()
+  max_pages<-as.numeric(max_pages[length(max_pages)-1])
+  print(max_pages)
+  #---------------------------------------------------------------------
+  
+  for(page_no in 1:max_pages)
+  {
+    exact_url<-paste(url_name,states[s_no],"/page/",page_no,"/",sep="")
+    if(page_no==1)
+    {
+      exact_url<-paste(url_name,states[s_no],"/",sep="")
+    }
+    print(paste("Accesing page number",page_no," url->",exact_url))
+    news_links<-html(exact_url)
+    links<-news_links %>%
+      html_nodes(".hentry .article-header a") %>%
+      html_attr("href")
+    
+    print(links)
+    links1<-c(links1,links)
+    print(length(links1))
+  }
+}
 
-half = length(links1)/2
-a = (1:half)
-a= 2*a
-a=a-1
-links1<-links1[a]
-
-print(links1)
-'''
 #---------------------------------------------------------------------
 
 #---------------------------ZeeNews-----------------------------------
-links2={}
-url_name<-"http://zeenews.india.com/state/north-east?page="
-for(page_no in 0:19)
-{
-  news_links<-html(paste(url_name,page_no,sep=""))
-  
-  links<-news_links %>%
-    html_nodes(".lead-health-ab a") %>%
-    html_attr("href")
-  for(i in 1:length(links))
-  {
-    links[i]=paste("http://zeenews.india.com",links[i],sep="")
-  }
-  links2<-c(links2,links)
-}
 
-print(links2)
+# links2={}
+# url_name<-"http://zeenews.india.com/state/north-east?page="
+# for(page_no in 0:19)
+# {
+#   news_links<-html(paste(url_name,page_no,sep=""))
+#   
+#   links<-news_links %>%
+#     html_nodes(".lead-health-ab a") %>%
+#     html_attr("href")
+#   for(i in 1:length(links))
+#   {
+#     links[i]=paste("http://zeenews.india.com",links[i],sep="")
+#   }
+#   links2<-c(links2,links)
+# }
+# 
+# print(links2)
+
 #---------------------------------------------------------------------
